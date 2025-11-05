8ddb4aed084114aae546a7730aa9262bd2b43635
gwrig007 <gwrig007@odu.edu>
2025-11-04 20:39:34 -0500

    updated verify-otp to go to the live server isntead of local

--- Project/client/src/js/verify-otp.js
+++ Project/client/src/js/verify-otp.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/user/verify-otp", {
+      const response = await fetch("https://cs418518-f25-1.onrender.com/user/verify-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, otp }),
